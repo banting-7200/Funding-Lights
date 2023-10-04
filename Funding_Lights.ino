cac2ef7401fa84e2db7e7a3d5bdae5cbbb0e494a
Logan Knott <91565741+Knott95@users.noreply.github.com>
2023-10-04 16:07:09 -0400

    Add files via upload

---
+++ Funding_Lights.ino
@@ -0,0 +1,20 @@
+#include <Adafruit_NeoPixel.h>
+
+int fundingGoal = 0; // The total ammount of money we wish to raise
+int sponsorMoney = 0, studentMoney = 0, msicMoney = 0; /* the amount of money currently contributed by sponsors, students and other sources. 
+                                                       Change the coressponding variable to change the ammount displayed on the light strip.*/
+// The pin the light string is attached to on the arduino
+#define LED_PIN 0 
+// The amount of LED's in the strip
+#define LEDCOUNT 248
+
+int buttonPin = 0; // The pin for the button which updates the lights
+void setup() {
+
+
+}
+
+void loop() {
+ 
+
+}
