e4cbaa5be53d573900d6ae34e26b0de407db6781
Logan Knott <91565741+Knott95@users.noreply.github.com>
2023-10-11 16:17:44 -0400

    Updated Funding_Lights_Array.ino

--- Funding_Lights_Array.ino
+++ Funding_Lights_Array.ino
@@ -1,10 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN 6    // Define the pin for your LED matrix
-#define NUM_LEDS 248  // Define the number of LEDs in your matrix
+#define NUM_LEDS 48/*248*/  // Define the number of LEDs in your matrix
 
 int fundingGoal = 24000; // The total ammount of money we wish to raise
-int sponsorMoney = 500, studentMoney = 300, msicMoney = 1000; /* the amount of money currently contributed by sponsors, students and other sources. 
+int sponsorMoney = 1000, studentMoney = 1000, msicMoney = 1000; /* the amount of money currently contributed by sponsors, students and other sources. 
                                                        Change the coressponding variable to change the ammount displayed on the light strip.*/
   int dollarsPerLED = fundingGoal / NUM_LEDS;// Calculate the ammount of money each LED represents
 
@@ -13,9 +13,9 @@ int buttonPin = 1; // The pin for the button which updates the lights
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-int DOT_COUNT = 0;  // Number of dots
+int DOT_COUNT = 50;  // Number of dots
 
-int dotPosition[DOT_COUNT][2];  // Array to store dot positions
+int dotPosition[50][2];  // Array to store dot positions
 float dotSpeed = 5;
 
 void setup() {
@@ -23,11 +23,11 @@ void setup() {
   strip.show();  // Initialize all pixels to 'off'
 
   for (int i = 0; i < DOT_COUNT; i++) {
-    dotPosition[i][0] = NUM_LEDS - 1;  // Start each dot on the right
-    dotPosition[i][1] = ;
+    //dotPosition[i][0] = NUM_LEDS - 1;  // Start each dot on the right
+    //dotPosition[i][1] = ;
   }
   
-  int funds[] = {(int)sponsorMoney / dollarsPerLED, (int)studentMoney / dollarsPerLED, (int)msicMoney / dollarsPerLED};
+  int funds[] = {round(sponsorMoney / dollarsPerLED), round(studentMoney / dollarsPerLED), round(msicMoney / dollarsPerLED)};
 
   DisplayLEDs(funds);
 }
@@ -54,12 +54,18 @@ void loop() {
 
 void DisplayLEDs(int funds[])
 {
-    for(int i = 0; i < funds.Length; i++)
-    {
-        for(int j = 0; j < funds[i]; j++)
+        int r = 50, g = 0, b = 0;
+        for(int j = 0; j < funds[0] + funds[1] + funds[2]; j++)
         {
-           strip.setPixelColor(j, strip.Color(40 * i, 0, 0));  // Red dot
-        }
-    }
-    strip.show();
+          if(j == funds[0])
+          {
+             r = 0; g = 50; b = 0;
+          }
+          else if(j == funds[0] + funds[1])
+          {
+             r = 0; g = 0; b = 50;
+          }
+           strip.setPixelColor(j, strip.Color(r, g, b));  // display the light
+           strip.show();
+        }      
 }
